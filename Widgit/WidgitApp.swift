6ad8d2de2a09b0fae02a16b04fee815f5641bfd4
Jacob Tepperman <jtepp@icloud.com>
2020-09-06 12:45:10 -0400

    Initial commit

---
+++ Widgit/WidgitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgitApp.swift
+//  Widgit
+//
+//  Created by Jacob Tepperman on 2020-09-06.
+//
+
+import SwiftUI
+
+@main
+struct WidgitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WidgitTests/WidgitTests.swift
@@ -0,0 +1,33 @@
+//
+//  WidgitTests.swift
+//  WidgitTests
+//
+//  Created by Jacob Tepperman on 2020-09-06.
+//
+
+import XCTest
+@testable import Widgit
+
+class WidgitTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
