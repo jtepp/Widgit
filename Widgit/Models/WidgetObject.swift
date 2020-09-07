50938d4ba8017a2947826032e0d824ecd259ec03
Jacob Tepperman <jtepp@icloud.com>
2020-09-06 22:02:25 -0400

    Added extra placeholders

--- Widgit/Models/WidgetObject.swift
+++ Widgit/Models/WidgetObject.swift
@@ -10,11 +10,13 @@ import SwiftUI
 
 
 
-struct WidgetObject: Identifiable {
+struct WidgetObject: Identifiable, Codable {
 	var id = UUID()
 	var sizeName: String
 	var width: CGFloat
 	var height: CGFloat
-	static let placeholder = WidgetObject(sizeName: "Small", width: 100, height: 100)
+	static let placeholder = WidgetObject(sizeName: "[Small]", width: 100, height: 100)
+	static let placeholderM = WidgetObject(sizeName: "[Medium]", width: 250, height: 100)
+	static let placeholderL = WidgetObject(sizeName: "[Large]", width: 250, height: 250)
 }
 
